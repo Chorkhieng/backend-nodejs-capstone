703df79b5c708c6f0be34bde3e28eaef13a4e2d5
Chork Hieng <hiengchorknu@gmail.com>
2025-03-31 19:18:12 -0500

    change init var

--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -13,7 +13,7 @@ router.get('/', async (req, res, next) => {
 
     // Initialize the query object
     // eslint-disable-next-line no-unused-vars
-    let query = { name: '', category: '', condition: '', age_years: '' }
+    const query = {}
 
     // Add the name filter to the query if the name parameter is not empty
     if (req.query.name && req.query.name.trim() !== '') {
