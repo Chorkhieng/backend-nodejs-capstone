d0c806d3cf441edd5341193f7a43ddba5d050108
Chork Hieng <hiengchorknu@gmail.com>
2025-03-31 19:32:59 -0500

    more linting index.js

--- sentiment/index.js
+++ sentiment/index.js
@@ -35,17 +35,17 @@ app.post('/sentiment', async (req, res) => {
 
     // Task 5: set sentiment to negative or positive based on score rules
     if (analysisResult < 0) {
-        sentiment = 'negative'
+      sentiment = 'negative'
     } else if (analysisResult > 0 && analysisResult < 0.33) {
-        sentiment = 'neutral'
+      sentiment = 'neutral'
     } else {
-        sentiment = 'positive'
+      sentiment = 'positive'
     }
 
-  // Logging the result
-  logger.info(`Sentiment analysis result: ${analysisResult}`)
-  // Task 6: send a status code of 200 with both sentiment score and the sentiment txt in the format { sentimentScore: analysisResult, sentiment: sentiment }
-  res.status(200).json({ sentimentScore: analysisResult, sentiment })
+    // Logging the result
+    logger.info(`Sentiment analysis result: ${analysisResult}`)
+    // Task 6: send a status code of 200 with both sentiment score and the sentiment txt in the format { sentimentScore: analysisResult, sentiment: sentiment }
+    res.status(200).json({ sentimentScore: analysisResult, sentiment })
   } catch (error) {
     logger.error(`Error performing sentiment analysis: ${error}`)
     // Task 7: if there is an error, return a HTTP code of 500 and the json {'message': 'Error performing sentiment analysis'}
